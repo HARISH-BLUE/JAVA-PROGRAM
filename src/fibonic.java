dc2b724ba393e032e71387a01750020267bb4cae
harish-blue <muthuselvamharish62@gmail.com>
2026-08-28 18:04:59 +0530

    added fibonic

---
+++ src/fibonic.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class fibonic {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int f=0;
+        int l=1;
+        int s=0;
+        for(int i=0;i<n;i++){
+            System.out.print(f+" ");
+            s=l+f;
+            f=l;
+            l=s;
+        }
+    }    
+}
