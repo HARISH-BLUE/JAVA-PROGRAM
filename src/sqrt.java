43b7f26d6c7094dc5a047b9198428c0cef815f57
harish-blue <muthuselvamharish62@gmail.com>
2026-08-31 22:16:46 +0530

    Add: Implement sqrt class to calculate the square root of a number

---
+++ src/sqrt.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class sqrt {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        double n=sc.nextDouble();
+        double result=Math.sqrt(n);
+        System.out.println("Square root of "+n+" is "+result);
+    }
+}
