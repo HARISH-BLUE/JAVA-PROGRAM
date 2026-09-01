14da9abf4a27dd7d80d47ef18d2a7597bb7bbb3c
harish-blue <muthuselvamharish62@gmail.com>
2026-09-01 18:35:25 +0530

    Add: Implement dsa2 class to read integers, sort digits, and print them

---
+++ src/dsa2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class dsa2 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String s = String.valueOf(n);
+        char[] digits = s.toCharArray();
+        Arrays.sort(digits);
+        int result = Integer.parseInt(new String(digits));
+        System.out.println(result);
+    }    
+}
