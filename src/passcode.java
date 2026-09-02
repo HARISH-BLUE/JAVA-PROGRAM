8c71e1231ce1eeb4eb1bef56afb267536fce6d92
harish-blue <muthuselvamharish62@gmail.com>
2026-09-02 22:40:23 +0530

    Add: Implement passcode class to read two integers and print their concatenated value

---
+++ src/passcode.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class passcode {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        String s=String.valueOf(n1)+String.valueOf(n2);
+        int n=Integer.parseInt(s);
+        System.out.print(n);
+    }    
+}
