2b611d6929418dcc1d3af2ea6c2c7332f65afc31
harish-blue <muthuselvamharish62@gmail.com>
2026-09-02 22:54:58 +0530

    Add: Implement testing class to read two integers and print their concatenated value

---
+++ src/testing.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class testing {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        // only for 2 dig concat
+        System.out.print(n1*100+n2);
+    }    
+}
