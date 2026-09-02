c42e3ca8ac352df7249528cbf1e7c2ad7e5ce8b0
harish-blue <muthuselvamharish62@gmail.com>
2026-09-02 22:40:59 +0530

    Update: Close scanner after reading input in passcode class

--- src/passcode.java
+++ src/passcode.java
@@ -4,8 +4,10 @@ public class passcode {
         Scanner sc=new Scanner(System.in);
         int n1=sc.nextInt();
         int n2=sc.nextInt();
+        sc.close();
         String s=String.valueOf(n1)+String.valueOf(n2);
         int n=Integer.parseInt(s);
         System.out.print(n);
+        
     }    
 }
