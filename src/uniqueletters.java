4557ff6b21a559b31fd12593d0db5855728703bf
harish-blue <muthuselvamharish62@gmail.com>
2026-09-03 18:17:23 +0530

    Update: Close scanner after reading input in uniqueletters class

--- src/uniqueletters.java
+++ src/uniqueletters.java
@@ -3,6 +3,7 @@ public class uniqueletters {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         String s=sc.next();
+        sc.close();
         String r="";
         for(int i=0;i<s.length();i++){
             if(!r.contains(String.valueOf(s.charAt(i)))){
