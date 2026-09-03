8a6e234907d266760eb92c572ab152332c387f35
harish-blue <muthuselvamharish62@gmail.com>
2026-09-03 18:37:25 +0530

    Update: Close scanner after reading input in dsa1 and dsa2 classes

--- src/dsa1.java
+++ src/dsa1.java
@@ -6,7 +6,8 @@ public class dsa1 {
         int[] arr=new int[n];
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
-        }for(int i=0;i<n-1;i++){
+        }sc.close();
+        for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
                 if(arr[i]>arr[j]){
                     arr[i]=arr[i]-arr[j];
--- src/dsa2.java
+++ src/dsa2.java
@@ -4,6 +4,7 @@ public class dsa2 {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        sc.close();
         String s = String.valueOf(n);
         char[] digits = s.toCharArray();
         Arrays.sort(digits);
