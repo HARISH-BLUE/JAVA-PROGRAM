b8093b1ee012c37223454fb33e57902309c07df9
harish-blue <muthuselvamharish62@gmail.com>
2026-09-16 18:06:37 +0530

    Update alphafirst class to read input from user instead of hardcoded string

--- src/alphafirst.java
+++ src/alphafirst.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class alphafirst {
 	public static void main(String[] args) {
-		String input = "a1b2c3";
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a string: ");
+		String input = scanner.nextLine();
 		StringBuilder alphabets = new StringBuilder();
 		StringBuilder remaining = new StringBuilder();
 
