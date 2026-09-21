b69672e4e61163d0bc6d1de4909f987a47cc52df
harish-blue <muthuselvamharish62@gmail.com>
2026-09-21 19:06:17 +0530

    Update Heartpattern.java size, add new classes for contains, swap, and alph; remove jam.java

---
+++ src/recurtion.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class recurtion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        hell(n, 1);
+        sc.close();
+    }
+
+    public static void hell(int n, int a) {
+        if (a > n) {
+            return;
+        }
+        System.out.print(a + " ");
+        hell(n, a + 1);
+    }
+}
