54d4199b2ec60f5081513ddce400d8a282233594
harish-blue <muthuselvamharish62@gmail.com>
2026-09-25 19:32:22 +0530

    Refactor GCD calculation into GCDandLCM.java and remove old gcd.java

--- src/GCDandLCM.java
+++ src/GCDandLCM.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
-public class gcd {
+public class GCDandLCM {
     public static void main(String[] args){
     
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
+        int num1=a;
+        int num2=b;
     while (b != 0) {
         int remainder = a % b;
         a = b;
         b = remainder;
     }
-    System.out.print(a);
+    System.out.println(a);
+    int lcm = (num1 / a) * num2;
+    System.out.print(lcm);
 }
 
 }
